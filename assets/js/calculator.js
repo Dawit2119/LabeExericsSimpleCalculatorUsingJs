bf898d612305b2a361e20009446ad310aa4655da
= <=>
2021-01-26 22:32:37 +0300

    making the rest functions and adding the services

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -45,55 +45,61 @@
             let total = add(arrayOfNumbers)
             return total/arrayOfNumbers.length
         }
-        
-        var choice = parseInt(prompt("<--------- A mini calculator--------->\n1: to add\n2: to subtract\n3: to multipy\n4: to divide \n5: to get maximum \n6: to  get minimum \n7: to square\n8:To get Average"))
+        var choice;
+        function getUserChoice(){
+            choice =  parseInt(prompt("<--------- A mini calculator--------->\n1: to add\n2: to subtract\n3: to multipy\n4: to divide \n5: to get maximum \n6: to  get minimum \n7: to square\n8:To get Average"))
+            return choice;
+        }
+            
         function createArray(){
-            arrayOfNumbers  = [];
-            numberValue = parseInt(prompt("enter the number of values you want to Compute"));
+            numberValue = parseInt(prompt("Enter the numbers separeted by comma or space"));
             for(i=0;i<numberValue;i++){
                 arrayOfNumbers[i] =parseInt(prompt("Enter the " +(i+1) + " element"))
             };
             return arrayOfNumbers;
         }
+        getUserChoice()
+        while(choice){
         switch(choice){       
             case 1: {
                 alert("sum = " + add(createArray()));
+                getUserChoice()
                 
             };
-            break
+            break;
             case 2:{
                 numberOne = parseInt(prompt("First number"))
                 numberTwo = parseInt(prompt("Second number"))
                 alert("difference = " + subtract(numberOne,numberTwo))
-                
+                getUserChoice()
             }break
             case 3:{
 
                 alert("product = " + multiply(createArray()))
-                
+                getUserChoice()
             }break
             case 4:{
                 numberOne = parseInt(prompt("First number"))
                 numberTwo = parseInt(prompt("Second number"))
                 alert("qutioent = " + divde(numberOne,numberTwo))
-                
+                getUserChoice()
             }break
             case 5: {
 
                 alert("The maximum value is " + max(createArray()));
-                
+                getUserChoice()
 
             }break
                             
             case 6: {
                 alert("The minimum value is "  + min(createArray()));
-                
+                getUserChoice()
             }break
             case 7: {
                 number = parseInt(prompt("enter number"))
                     alert("the square of " + number +" = " + square(number))
                     
-                
+                    getUserChoice()
             }break
             case 8: {
                 alert("The average is " + average(createArray()))
@@ -103,4 +109,5 @@
             
             
         };
+    }
 })();
